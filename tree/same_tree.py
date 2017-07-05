1ff171f8d7889966cfce21ea1edaefe4a2a11839
plus <xjrs_zhangwj@163.com>
2017-07-05 11:13:06 +0800

    modify same tree

--- tree/same_tree.py
+++ tree/same_tree.py
@@ -7,7 +7,23 @@ class Solution(object):
         :type q: TreeNode
         :rtype: bool
         """
-        return self.is_same_recur(p,q)
+        # return self.is_same_recur(p,q)
+        queue1, queue2 = Queue.Queue(), Queue.Queue()
+        queue1.put(p)
+        queue2.put(q)
+        while (not  queue1.empty()) and (not queue2.empty()):
+            p_t, q_t = queue1.get(), queue2.get()
+            if (not p_t) and (not q_t):
+                continue
+            elif (not p_t) or (not q_t):
+                return False
+            elif p_t.val != q_t.val:
+                return False
+            queue1.put(p_t.left)
+            queue1.put(p_t.right)
+            queue2.put(q_t.left)
+            queue2.put(q_t.right)
+        return True
 
 
     def is_same_recur(self, p, q):
