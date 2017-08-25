75e4935407c2ad0112a8d2828a679eb3a7d26d6e
plus <460115153@qq.com>
2017-08-25 22:05:23 +0800

    add reverse linklist

--- link/reverse_link.py
+++ link/reverse_link.py
@@ -7,4 +7,27 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        if not head and
+        prev = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = prev
+            prev = curr
+        return prev
+
+    def reverse_recur(self, p, prev=None):
+        if not p:
+            return prev
+        follow = p.next
+        p.next = prev
+        self.reverse_recur(follow, p)
+
+    def reverse_recur_1(self, head):
+        if not head or not head.next:
+            return head
+        # reverse following node after head
+        new_head = self.reverse_recur_1(head.next)
+        # then reverse head
+        head.next.next = head
+        head.next = None
+        return new_head
